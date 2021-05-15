6f2c270ccda61a06f3f0f5b26c71bcb6e42d7fe9
Noah Herron <jayssbronze@gmail.com>
2021-05-15 14:43:36 -0400

    started format/error handling, formatted some output

--- src/main.py
+++ src/main.py
@@ -1,28 +1,52 @@
 
 import CreditCard as CC
 
-cardCount = int(input("How many credit cards do you have? "))
+def handleInput(message):
+	value = input(message)
+	print()
+	return value
+
+def validatePricipal(principal):
+	if principal <= 0:
+		print("Please enter a non-zero positive value for the principal.")
+		return False
+	return True
+
+def validateMonths(months):
+	if months <= 0:
+		print("Please enter a non-zero positive value for the months.")
+		return False
+	return True
+
+MONEY_FORMAT = "{:5.2f}"
+
+cardCount = int(handleInput("How many credit cards do you have? "))
 cards = []
-print()
+print("==================================\n")
 
 # todo nice error handling for illegal inputs
 for i in range(cardCount):
-	name = input("What is the name of this card? ")
-	principal = float(input("What's your principal? "))
-	rate = float(input("What's your APR? "))
-	timeYears = float(int(input("How many months? ")) / 12.0)
+	name = handleInput("What is the name of this card? ")
+	principal = 0
+	while validatePricipal(principal) == False:
+		principal = float(handleInput("What's this card's principal? $ "))
+	rate = float(handleInput("What's this card's APR? ")) #todo check format
+	months = 0
+	while validateMonths(months) == False:
+		months = int(handleInput("How many months to pay off this card? "))
+	timeYears = float(months / 12.0)
 	cards.append(CC.CreditCard(principal, rate, timeYears, name))
-	print() # new line
+	print("==================================\n")
 
 grandTotal = 0
 for card in cards:
 	# check format of rate - we want it to be a decimal i.e. 50% == .5
 	card.interest = float(card.principal * card.rate * card.timeYears)
-	print("For card "+card.name+", you will pay this amount in interest:$ " + "{:5.2f}".format(card.interest))
+	print("For card "+card.name+", you will pay this amount in interest:$ " + MONEY_FORMAT.format(card.interest))
 
 	total = float(card.principal + card.interest)
-	print("\tand you will pay this amount in total:$ " + "{:5.2f}".format(total))
+	print("\tand you will pay this amount in total:$ " + MONEY_FORMAT.format(total))
 	grandTotal += total
 	print()
 
-print("For all " + str(cardCount) + " cards, you will pay:$ " + "{:5.2f}".format(grandTotal))
+print("For all " + str(cardCount) + " cards, you will pay:$ " + MONEY_FORMAT.format(grandTotal))
